cb300f1a0d1b07e39c4d020adb8482a59adda8c3
XiaodongXie <xiexiaodong917@163.com>
2017-03-13 16:00:33 +0800

    :art:格式化代码

--- xmail.go
+++ xmail.go
@@ -1,8 +1,8 @@
 package xmail
 
 import (
-    "strings"
-    "net/smtp"
+	"net/smtp"
+	"strings"
 )
 
 //描述：发送电子邮件方法
@@ -15,16 +15,16 @@ import (
 //  content：邮件内容
 //  mailType：邮件类型
 func Send(fromUser, password, host, toUsers, title, content, mailType string) error {
-    hp := strings.Split(host, ":")
-    auth := smtp.PlainAuth("", fromUser, password, hp[0])
-    var contentType string
-    if mailType == "html" {
-        contentType = "Content-Type: text/" + mailType + "; charset=UTF-8"
-    } else {
-        contentType = "Content-Type: text/plain" + "; charset=UTF-8"
-    }
-    msg := []byte("To: " + toUsers + "\r\nFrom: " + fromUser + "<" + fromUser + ">\r\nSubject: " + title + "\r\n" + contentType + "\r\n\r\n" + content)
-    toUsersList := strings.Split(toUsers, ";")
-    err := smtp.SendMail(host, auth, fromUser, toUsersList, msg)
-    return err
+	hp := strings.Split(host, ":")
+	auth := smtp.PlainAuth("", fromUser, password, hp[0])
+	var contentType string
+	if mailType == "html" {
+		contentType = "Content-Type: text/" + mailType + "; charset=UTF-8"
+	} else {
+		contentType = "Content-Type: text/plain" + "; charset=UTF-8"
+	}
+	msg := []byte("To: " + toUsers + "\r\nFrom: " + fromUser + "<" + fromUser + ">\r\nSubject: " + title + "\r\n" + contentType + "\r\n\r\n" + content)
+	toUsersList := strings.Split(toUsers, ";")
+	err := smtp.SendMail(host, auth, fromUser, toUsersList, msg)
+	return err
 }
--- xmail_test.go
+++ xmail_test.go
@@ -3,10 +3,10 @@ package xmail
 import "testing"
 
 // 测试Send()方法发送邮件
-func TestSend(t *testing.T)  {
-    if err := Send("770****12@qq.com", "yrg********ddf", "smtp.qq.com:25", "1****48@qq.com;1*****43@qq.com", "邮件标题", "邮件内容", "html"); err != nil {
-        t.Error("发送邮件失败：", err.Error())
-    } else {
-        t.Log("发送邮件成功！")
-    }
+func TestSend(t *testing.T) {
+	if err := Send("770****12@qq.com", "yrg********ddf", "smtp.qq.com:25", "1****48@qq.com;1*****43@qq.com", "邮件标题", "邮件内容", "html"); err != nil {
+		t.Error("发送邮件失败：", err.Error())
+	} else {
+		t.Log("发送邮件成功！")
+	}
 }
